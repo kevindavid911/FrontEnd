0a33e3174addf9929ebbe8f12c9465a862108041
kevindavid911 <kevin.ameliano@gmail.com>
2025-04-08 11:51:44 +0700

    Progress Materials

---
+++ src/Materials/index.jsx
@@ -0,0 +1,253 @@
+import React from "react";
+import "./index.css";
+
+const Materials = () => {
+  return (
+    <div className="materials">
+      <div className="overlap">
+        <div className="rectangle" />
+
+        <div className="div" />
+
+        <div className="rectangle-2" />
+
+        <div className="text-wrapper">SIMPLE PAST TENSE</div>
+
+        <div className="text-wrapper-2">(WAKTU LAMPAU SEDERHANA)</div>
+
+        <div className="text-wrapper-3">1 hour</div>
+
+        <div className="text-wrapper-4"></div>
+
+        <div className="group">
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group">
+              <img
+                className="line"
+                alt="Line"
+                src="https://c.animaapp.com/oXjDRmRD/img/line-27.svg"
+              />
+
+              <img
+                className="img"
+                alt="Line"
+                src="https://c.animaapp.com/oXjDRmRD/img/line-28.svg"
+              />
+
+              <img
+                className="line-2"
+                alt="Line"
+                src="https://c.animaapp.com/oXjDRmRD/img/line-29.svg"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="text-wrapper-5">Change Simplification</div>
+
+        <div className="text-wrapper-6">Pengertian Simple Past Tense</div>
+
+        <div className="text-wrapper-7">PRACTICE QUIZ</div>
+
+        <div className="rectangle-3" />
+
+        <div className="text-wrapper-8"></div>
+
+        <p className="p">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-9">30 mins</div>
+
+        <div className="rectangle-4" />
+
+        <div className="text-wrapper-10"></div>
+
+        <p className="text-wrapper-11">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-12">30 mins</div>
+
+        <div className="rectangle-5" />
+
+        <div className="text-wrapper-13"></div>
+
+        <p className="text-wrapper-14">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-15">30 mins</div>
+
+        <div className="rectangle-6" />
+
+        <div className="text-wrapper-16"></div>
+
+        <p className="text-wrapper-17">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-18">30 mins</div>
+
+        <div className="rectangle-7" />
+
+        <div className="text-wrapper-19"></div>
+
+        <p className="text-wrapper-20">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-21">30 mins</div>
+
+        <div className="rectangle-8" />
+
+        <div className="text-wrapper-22"></div>
+
+        <p className="text-wrapper-23">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-24">30 mins</div>
+
+        <div className="rectangle-9" />
+
+        <div className="text-wrapper-25"></div>
+
+        <p className="text-wrapper-26">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-27">30 mins</div>
+
+        <div className="rectangle-10" />
+
+        <div className="text-wrapper-28"></div>
+
+        <p className="text-wrapper-29">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-30">30 mins</div>
+
+        <div className="rectangle-11" />
+
+        <div className="text-wrapper-31"></div>
+
+        <p className="text-wrapper-32">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-33">30 mins</div>
+
+        <div className="rectangle-12" />
+
+        <div className="text-wrapper-34"></div>
+
+        <p className="text-wrapper-35">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-36">30 mins</div>
+
+        <div className="rectangle-13" />
+
+        <div className="text-wrapper-37"></div>
+
+        <p className="text-wrapper-38">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-39">30 mins</div>
+
+        <div className="rectangle-14" />
+
+        <div className="text-wrapper-40"></div>
+
+        <p className="text-wrapper-41">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-42">30 mins</div>
+
+        <div className="rectangle-15" />
+
+        <div className="text-wrapper-43"></div>
+
+        <p className="text-wrapper-44">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-45">30 mins</div>
+
+        <div className="rectangle-16" />
+
+        <div className="text-wrapper-46"></div>
+
+        <p className="text-wrapper-47">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-48">30 mins</div>
+
+        <div className="rectangle-17" />
+
+        <div className="text-wrapper-49"></div>
+
+        <p className="text-wrapper-50">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-51">30 mins</div>
+
+        <div className="rectangle-18" />
+
+        <div className="text-wrapper-52"></div>
+
+        <p className="text-wrapper-53">Lesson 01 : Introduction to Grammar</p>
+
+        <div className="text-wrapper-54">30 mins</div>
+
+        <p className="text-wrapper-55">
+          Simple Past Tense adalah bentuk kata kerja yang digunakan untuk
+          menyatakan suatu tindakan atau kejadian yang terjadi di masa lalu dan
+          sudah selesai. Tense ini sering digunakan dalam narasi atau cerita
+          untuk menggambarkan peristiwa lampau
+        </p>
+
+        <div className="rectangle-19" />
+
+        <p className="text-wrapper-56">
+          Fungsi dan Penggunaan Simple Past Tense
+        </p>
+
+        <p className="text-wrapper-57">
+          a. Menyatakan Kejadian yang Terjadi dan Selesai di Masa Lalu
+        </p>
+
+        <div className="rectangle-20" />
+
+        <p className="digunakan-untuk">
+          Digunakan untuk peristiwa yang terjadi pada waktu tertentu di masa
+          lalu.
+          <br />
+          Contoh:
+          <br />
+          She visited her grandmother last week. (Dia mengunjungi neneknya
+          minggu lalu.)
+          <br />
+          They watched a movie yesterday. (Mereka menonton film kemarin.)
+        </p>
+
+        <div className="rectangle-21" />
+
+        <p className="text-wrapper-58">O6 Super Coins on the way</p>
+
+        <p className="text-wrapper-59">
+          Lorem ipsum dolor sit amet, consectetur adi
+        </p>
+
+        <div className="rectangle-22" />
+
+        <p className="text-wrapper-60">
+          Lorem ipsum dolor sit amet, consectetur adi piscing elit, sed do
+          eiusmodadipiscing elit, sed do eiusmodLorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmodadipiscing elit, sed do
+          eiusmodLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmodadipiscing elit, sed do eiusmodeiusmodadipiscing elit, sed do
+          eiusmodL
+        </p>
+
+        <div className="rectangle-23" />
+
+        <p className="text-wrapper-61">O6 Super Coins on the way</p>
+
+        <p className="text-wrapper-62">
+          Lorem ipsum dolor sit amet, consectetur adi
+        </p>
+
+        <div className="rectangle-24" />
+
+        <p className="text-wrapper-63">
+          Lorem ipsum dolor sit amet, consectetur adi piscing elit, sed do
+          eiusmodadipiscing elit, sed do eiusmodLorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmodadipiscing elit, sed do
+          eiusmodLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmodadipiscing elit, sed do eiusmodeiusmodadipiscing elit, sed do
+          eiusmodL
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Materials;
